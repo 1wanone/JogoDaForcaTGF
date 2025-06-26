37097f0b573d3dd554ee7b38ba496bbaaf9d5659
wvict <wvictor665@gmail.com>
2025-06-25 22:14:26 -0300

    final

--- src/DAO/Conexao.java
+++ src/DAO/Conexao.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class Conexao {
     private static final String URL = "jdbc:mysql://127.0.0.1:3306/jogodaforca";
     private static final String USUARIO = "root";
-    private static final String SENHA = "99659065"; // <-- Altere para sua senha do MySQL
+    private static final String SENHA = "99659065";
 
     public static Connection getConexao() throws SQLException {
         return DriverManager.getConnection(URL, USUARIO, SENHA);
